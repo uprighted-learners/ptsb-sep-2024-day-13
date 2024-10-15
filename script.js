d5e74bd8effad73f98c140f64f4fcb1acd6a223e
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-15 17:48:43 -0500

    var let const examples

--- script.js
+++ script.js
@@ -27,6 +27,31 @@ showGlobalVar() // I am a GLOBAL variable!
 console.log("console log", globalVar) // I am a GLOBAL variable!
 console.log("console log", anotherGlobal) // I am another global variable!
 // console.log("console log", localVar) // localVar is not defined
-console.log("console log", localVarVariable) // localVarVariable is not defined
+// console.log("console log", localVarVariable) // localVarVariable is not defined
 console.log("console log", localVarVarVariable) // I am a var global variable!
 
+function testVar() {
+    var x = 10;
+    if (true) {
+        var x = 20;
+        console.log(x); // 20
+    }
+    console.log(x); // 20
+}
+
+testVar()
+
+function testLet() {
+    let x = 10;
+    if (true) {
+        let x = 20;
+        console.log(x); // 20
+    }
+    console.log(x); // 10
+}
+testLet()
+
+const PI = 3.14159;
+console.log("PI", PI)
+PI = 3.14; // Uncaught TypeError: Assignment to constant variable.
+console.log("PI again", PI)
