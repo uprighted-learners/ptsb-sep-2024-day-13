7873e75ca404eefe29c1eb398659165f697dcf0d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-10-15 19:42:09 -0500

    99 bottles of beer

--- script.js
+++ script.js
@@ -96,9 +96,64 @@ function cleanCodeExample() {
 cleanCodeExample()
 
 // functional expression - which is a NO NO on HOISTING - it will not automatically work for you!
-anotherHoistedFunction()
+// anotherHoistedFunction()
 
 let anotherHoistedFunction = function () {
     console.log("I am clean code hoisted function - the very best!")
 }
 
+
+// RECURSION
+function factorial(n) {
+    // base case: if n is 1, return 1
+    if (n === 1) {
+        return 1;
+    }
+
+    // recursive case: n * factorial(n - 1)
+    return n * factorial(n - 1);
+};
+
+console.log("factorial", factorial(10))
+
+function infiniteRecursion() {
+    infiniteRecursion()
+}
+
+// infiniteRecursion() // ERROR: Maximum call stack size exceeded
+
+function fibonacci(n) {
+    // base case: if n is 0 or 1, return n
+    if (n === 0) {
+        return n;
+    } else if (n === 1) {
+        return n;
+    }
+
+    // recursive case: fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log("fibonacci", fibonacci(25))
+
+function singBeerSong(bottles) {
+    let initialValue = bottles
+
+    // base case: if bottles is 0, return "No more bottles of beer on the wall"
+    if (bottles === 0) {
+        console.log("No more bottles of beer on the wall, no more bottles of beer.")
+        console.log(`Go to the store and buy some more, ${initialValue} bottles of beer on the wall.`)
+        return; // return is optional here
+    }
+
+    // print the current verse
+    console.log(`${bottles} bottle${bottles === 1 ? "" : "s"}` + " of beer on the wall")
+    console.log(`${bottles} bottle${bottles === 1 ? "" : "s"} of beer`)
+    console.log("Take one down, pass it around.")
+    console.log((bottles - 1) + " bottles of beer on the wall.")
+    console.log("")
+
+    // recursive case
+    singBeerSong(bottles - 1)
+};
+
+singBeerSong(9)
